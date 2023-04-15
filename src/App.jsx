a8189fd75232af58a55f3b20fc68e0edf6cf9435
bothuany <rbdikmen@gmail.com>
2023-04-15 18:49:01 +0300

    Installed tailwindcss, created-homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import HomePage from './views/HomePage';
 
 function App() {
   return (
     <div className="App">
       <h1>E-Commerce App</h1>
+      <HomePage></HomePage>
+      
+      
     </div>
   );
 }
