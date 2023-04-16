80dcf06043fd638f55b409681b14c078f6833fb3
bothuany <rbdikmen@gmail.com>
2023-04-16 03:15:24 +0300

    Router and layout added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Products from '../components/Products'
 
-function HomePage() {
+function Home() {
     
   return (
     <Products></Products>
   )
 }
 
-export default HomePage
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html",
-
-
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
 
