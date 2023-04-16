6355a13c4e67d5a8c1ff6912dd915979b041ee2b
bothuany <rbdikmen@gmail.com>
2023-04-16 19:13:16 +0300

    cart added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Product({product}) {
   return (
@@ -14,10 +15,10 @@ function Product({product}) {
         <div className="mt-4 flex justify-between">
             <div>
                 <h3 className="text-sm text-gray-700">
-                <a href={product.href}>
+                <Link to={product.href}>
                     <span aria-hidden="true" className="absolute inset-0" />
                     {product.name}
-                </a>
+                </Link>
                 </h3>
                 <p className="mt-1 text-sm text-gray-500">{product.color}</p>
             </div>
